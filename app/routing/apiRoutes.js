477934aafbc8356722269009203be52b7738bade
Morgan Gonzalez <Morgan@morgans-mbp.attlocal.net>
2017-01-22 17:42:03 -0500

    fix

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,54 +1,46 @@
 var elementData = require('../data/elements.js');
 var path = require('path');
-
+var elementScores = [];
 
 module.exports = function(app){
-	//app.get('/api/elements', function(req, res){
-		//res.json(elementData);
-	//});
+	
 
 //API POST Request-handles when user submits a form & thus submits data to the server.
 // In each of the below cases, when a user submits form data (a JSON object)
 // ...the JSON is pushed to the appropriate Javascript array
 
+	 app.get("/api/elements", function(req, res) {
+    	res.json(elementData);
+  	});
+
+	 for (var i=0; i < elementData.length; i++){
+	 	if (elementScores <= 15) {
+	 		//show rocks/ground
+	 	/*} else if { (elementScores = 16-24)
+	 		//show mountains
+	 	} else if { (elementScores = 25-34)
+	 		//show wind
+	 	} else if { (elementScores = 35-44)
+	 		//show fire
+	 	} else if { (elementScores = 45-50)
+	 		//show water
+	 	} */
+	 		
+	 };
+
 
 	app.post('/api/elements', function(req, res){
 
-		var greatMatch = {
-			name: "",
-			image: "",
-			matchDifference: 1000
-		};
-		var usrData 	= req.body;
-		var usrName 	= usrData.name;
-		var usrImage 	= usrData.image;
-		var usrScores 	= usrData.scores;
-
-		var totalDifference = 0;
-
-		//loop through the friends data array of objects to get each friends scores
-		for(var i = 0; i < elementData.length-1; i++){
-			console.log(elementData[i].name);
-			totalDifference = 0;
-
-			//loop through that friends score and the users score and calculate the 
-			// absolute difference between the two and push that to the total difference variable set above
-			for(var j = 0; j < 10; j++){
-				// We calculate the difference between the scores and sum them into the totalDifference
-				totalDifference += Math.abs(parseInt(usrScores[j]) - parseInt(elementData[i].scores[j]));
-				// If the sum of differences is less then the differences of the current "best match"
-				if (totalDifference <= greatMatch.elementDifference){
-
-					// Reset the bestMatch to be the new friend. 
-					greatMatch.name = elementData[i].name;
-					greatMatch.photo = elementData[i].photo;
-					greatMatch.matchDifference = totalDifference;
-				}
-			}
-		}
+		
 
 		elementData.push(usrData);
  
 		res.json(greatMatch);
 	});
-};
+
+
+	};
+
+}
+
+
